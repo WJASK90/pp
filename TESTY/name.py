ca0ba4dc3b4b1374e5e7863e24835b0a9d4edba1
WJASK90 <jaskot.w92@gmail.com>
2024-10-27 20:02:51 +0100

    testy

---
+++ TESTY/name.py
@@ -0,0 +1,19 @@
+name = "jan kowalski"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "jan"
+last_name = "kowalski"
+full_name = f"{first_name} {last_name}"
+print(f"Witaj, {full_name.title()}!")
+
+first_name = "jan"
+last_name = "kowalski"
+full_name = f"{first_name} {last_name}"
+message = f"Witaj, {full_name.title()}!"
+print(message)
+
+print("\tPython")
+print("Języki programowania:\nPython\nC++\nJava")
+print("Języki programowania:\n\tPython\n\tC++\n\tJava")
