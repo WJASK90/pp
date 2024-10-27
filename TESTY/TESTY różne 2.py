95ba6899e8990badfeb81a6b3eca20e0ecec30ff
WJASK90 <jaskot.w92@gmail.com>
2024-10-27 15:08:45 +0100

    testy

--- TESTY/TESTY różne 2.py
+++ TESTY/TESTY różne 2.py
@@ -1,3 +1,8 @@
 print("Magda")
 print("Wojtek")
 print("dom")
+message = "Witaj w świecie Pythona!"
+print(message)
+
+message = 'Witaj, świecie książki "Python. Instrukcje dla programisty"!'
+print(message)
