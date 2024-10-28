a206d421b1be3d6826c26f30f25496c8e85c6619
WJASK90 <jaskot.w92@gmail.com>
2024-10-28 18:27:50 +0100

    testy

--- TESTY/Zadanie domowe z 05-06.11.24.py
+++ TESTY/Zadanie domowe z 05-06.11.24.py
@@ -22,7 +22,7 @@ print("Wartosc z oprocentowania 7,5% po 3Q", 30000 * 1.01875 * 1.01875 * 1.01875
 print("Wartosc z oprocentowania 7,5% po 4Q", 30000 * 1.01875 * 1.01875 * 1.01875 * 1.01875, "zł")
 #po roku mamy 32314.075973510742 zł
 rok_1 = 32314.075973510742
-print(rok_1 - start) #zysk z całego roku z oprocentowania 7,5%
+print(round(rok_1 - start)) #zysk z całego roku z oprocentowania 7,5%
 
 #oprocentowanie 8%, na trzy kwartaly
 print("Wartosc z oprocentowania 8% po 1Q", 30000 * 1.02, "zł")
@@ -31,7 +31,7 @@ print("Wartosc z oprocentowania 8% po 3Q", 30000 * 1.02 * 1.02 * 1.02, "zł")
 print("Wartosc z oprocentowania 8% po 4Q", 30000 * 1.02 * 1.02 * 1.02 * 1.02, "zł")
 #po roku mamy 32472.9648 zł
 rok_2 = 32472.9648
-print(rok_2 - start) #zysk z całego roku z oprocentowania 8%
+print(round(rok_2 - start)) #zysk z całego roku z oprocentowania 8%
 
 #oprocentowanie 8.25%, na trzy kwartaly
 print("Wartosc z oprocentowania 8,25% po 1Q", 30000 * 1.020625, "zł")
@@ -40,6 +40,6 @@ print("Wartosc z oprocentowania 8,25% po 3Q", 30000 * 1.020625 * 1.020625 * 1.02
 print("Wartosc z oprocentowania 8,25% po 4Q", 30000 * 1.020625 * 1.020625 * 1.020625 * 1.020625, "zł")
 #po roku mamy 32552.628583012374 zł
 rok_3 = 32552.628583012374
-print(rok_3 - start) #zysk z całego roku z oprocentowania 8,25%
+print(round(rok_3 - start)) #zysk z całego roku z oprocentowania 8,25%
 
 
