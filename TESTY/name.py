2a1e1ede9b5f7cf5e7fbcd13dfa9df9da757cffa
WJASK90 <jaskot.w92@gmail.com>
2024-10-29 19:20:57 +0100

    testy

--- TESTY/name.py
+++ TESTY/name.py
@@ -17,3 +17,16 @@ print(message)
 print("\tPython")
 print("Języki programowania:\nPython\nC++\nJava")
 print("Języki programowania:\n\tPython\n\tC++\n\tJava")
+
+favorite_language = ' python '
+favorite_language = favorite_language.rstrip()
+favorite_language = favorite_language.lstrip()
+favorite_language = favorite_language.strip()
+print(favorite_language)
+
+nostarch_url = 'https://nostarch.com'
+simple_url = nostarch_url.removeprefix('https://')
+print(simple_url)
+
+message = "Dla programisty Johna O'Hary jedną z zalet Pythona jest jego wszechstronność i oddana mu społeczność"
+print(message) 
