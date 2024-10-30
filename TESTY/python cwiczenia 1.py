3b48b96994cc4b15af109478cbb6e488ffacc4b7
WJASK90 <jaskot.w92@gmail.com>
2024-10-30 18:51:56 +0100

    testy

---
+++ TESTY/python cwiczenia 1.py
@@ -0,0 +1,27 @@
+#cwiczenie 1
+
+imie_a = "Magda"
+
+print("Cześć " + str(imie_a) + "!, Czy chcesz dzisiaj poznawać Pythona?")
+
+imie_b = "cymbała"
+
+print(imie_b.lower())
+print(imie_b.upper())
+print(imie_b.title())
+
+print('Alber Einstein powiedział kiedyś: "Osoba, która nigdy nie popełniła błedu, jest kimś, kto nigdy nie próbował czegoś nowego"')
+
+famous_person = "Albert Einstein"
+message = ' powiedział kiedyś: "Osoba, która nigdy nie popełniła błedu, jest kimś, kto nigdy nie próbował czegoś nowego"'
+famous_message = f"{famous_person}{message}"
+print(famous_message)
+
+print("\tZygmunt")
+print("Zygmunt\t")
+
+print("Zygmunt\t\nIrena\t\n")
+
+irena_1 = " Irena"
+irena_2 = "Irena "
+irena_3 = " Irena "
