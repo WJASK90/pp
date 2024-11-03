d2ba5abb6484e6de9c96db4dc4527efedeef5170
WJASK90 <jaskot.w92@gmail.com>
2024-11-03 16:43:24 +0100

    testy

--- TESTY/python cwiczenia 2.py
+++ TESTY/python cwiczenia 2.py
@@ -14,3 +14,23 @@ print(bicycles[0])
 print(bicycles[-2])
 print(bicycles[-1].upper())
 print(bicycles[1].title())
+
+message_a = f"Moim pierwszym rowerem, był rower {bicycles[0]}"
+print(message_a)
+
+#cwiczenia 3.1 + 3.2 + 3.3
+
+names = ['Wojtek', 'Magda', 'Radek']
+print(names[0])
+print(names[1])
+print(names[2])
+
+message_b = f"Witaj na imprezie, {names[0]}"
+print(message_b)
+message_c = f"Witaj na imprezie, {names[1]}"
+print(message_c)
+message_d = f"Witaj na imprezie, {names[2]}"
+print(message_d)
+
+motorcycles = ['Honda', 'KTM', 'Ducati']
+print("Chciałbym mieć samochód " + str(motorcycles[0]))
