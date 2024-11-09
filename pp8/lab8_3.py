89b8b6a614e87d3059ef14027aa97c566ce908dd
WJASK90 <jaskot.w92@gmail.com>
2024-11-09 14:20:28 +0100

    test2

--- pp8/lab8_3.py
+++ pp8/lab8_3.py
@@ -12,3 +12,16 @@ for i in range(64):
     s += 2 ** i #dodaj do sumy za kazdym raze 2 do potegi i
 
 print("Suma wszystkich ziaren zboża na szachownicy to " + str(s))
+
+
+# założenia: waga 1 ziarna to 0,4mg -> 0,0004g
+
+# 1kg = 1000g
+# 1000 kg = 1t
+
+tons = int(s * 0.0004 / 1000 / 1000)
+print("ton: ", tons)
+
+# roczna produkcja pszenicy na świecie to 782 mln ton
+years = int(tons / 782_000_000)
+print("lata: ", years)
