7cbb5002a68d13b9fb25546eb17707d7250b5141
WJASK90 <jaskot.w92@gmail.com>
2024-11-09 11:55:54 +0100

    test2

---
+++ pp8/demo PETLE.py
@@ -0,0 +1,17 @@
+i = 0
+
+while i < 10:
+    print(i, end=" ")
+    i += 1
+
+print()
+
+for i in range(0, 10):
+    print(i, end=" ")
+
+#for i in range(0, 10, 1): --> DOSTANIE TO SAMO CO NA DOLE z range(10)
+for i in range(10):
+    print(i)
+
+for i in range(3,10):
+    print(i)
