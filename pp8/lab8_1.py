1fafb928d216262fb3cd30384339898acb4e47d8
WJASK90 <jaskot.w92@gmail.com>
2024-11-09 12:55:00 +0100

    test2

--- pp8/lab8_1.py
+++ pp8/lab8_1.py
@@ -1,3 +1,4 @@
+#technika wykonywania cwiczen: rozdziel na etapy, dodawaj coraz bardziej skomplikowane kroki
 #Wyświetl liczby od 1 do 100 z pominięciem liczb podzielnych przez 3
 #zastosowałem modulo, używając // dzieliło wartości
 for i in range(0, 100):
--- pp8/lab8_2.py
+++ pp8/lab8_2.py
@@ -1,12 +1,22 @@
 #Napisz skrypt wyświetlający na ekranie macierz. Rozmiar macierzy oraz znak z jakiej będzie zbudowana powinien określić użytkownik
 
-size = int(input("Podaj rozmiar: "))
-sign = input("Podaj znak: ")
+# size = int(input("Podaj rozmiar: "))
+# sign = input("Podaj znak: ")
+#
+# i = (size * size) * sign
+#
+# for i in range(0, 6):
+#     print(i, end=" " * size )
 
-i = (size * size) * sign
+#print() uzywamy po to aby przejsc do nowej linii
 
-for i in range(0, 6):
-    print(i, end=" " * size )
+number = int(input("Podaj rozmiar: "))
+char = input("Podaj znak: ")
+
+for i in range(number):
+    for j in range(number):
+        print(char, end=" ")
+    print()
 
 
 
