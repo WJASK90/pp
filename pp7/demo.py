30a412d31011bf46e47562ade9a472daaf080229
WJASK90 <jaskot.w92@gmail.com>
2024-11-09 09:28:05 +0100

    test2

--- pp7/demo.py
+++ pp7/demo.py
@@ -1,13 +1,19 @@
 a, b = 6, 7
 
 print(a > b)
-
 print(2 == 2.)
 
-number = 6
+number = -3
 
 if number > 3:
     print("Liczba jest większa niż 3")
     print("Hurra!")
 print("Koniec")
+
+if number > 0:
+    print("Liczba większa od zera")
+elif number < 0:
+    print("Liczba mniejsza od zera")
+else:
+    print("Liczba równa zero")
 
