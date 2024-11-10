1d444f036c7dcf132d71c0ae08a7264f5ff922f2
WJASK90 <jaskot.w92@gmail.com>
2024-11-10 16:39:54 +0100

    testy

--- pp11/demo3.py
+++ pp11/demo3.py
@@ -5,4 +5,24 @@ print(matrix)
 matrix[0][0] = 99
 print(matrix)
 
+print("*" * 25)
 #wygeneruj plansze do szachownicy, czyli plansze 8 * 8 (64 pola)
+
+# tworzymy planszę do gry w szachy, czyli szachownice (OMG)
+
+# chess_row = ["--", "--", "--", "--", "--", "--", "--", "--"]
+# chess_row = ["--" for _ in range(8)]
+# chessboard = [chess_row[:] for _ in range(8)]
+
+chessboard = [["--" for _ in range(8)] for _ in range (8)]
+
+WHITE_PAWN = "BP"
+BLACK_PAWN = "CP"
+
+chessboard[3][4] = WHITE_PAWN
+chessboard[2][7] = BLACK_PAWN
+
+for chess_row in chessboard:
+    for chess_square in chess_row:
+        print(chess_square, end=" ")
+    print()
