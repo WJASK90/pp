a2bcc5ead324e998e0014f6ff6251331e458935d
WJASK90 <jaskot.w92@gmail.com>
2024-11-21 14:20:39 +0100

    testy

--- TESTY/Zadanie domowe z 09-10.11.24.py
+++ TESTY/Zadanie domowe z 09-10.11.24.py
@@ -3,6 +3,12 @@
 # • niepodanie liczby powinno zakończyć wprowadzanie danych,
 # • program powinien podać sumę liczb parzystych oraz sumę liczb nieparzystych.
 
+
+#czyli musze zrobic tak: musze pytac uzytkownika o liczby calkowite i jak juz bedzie chcial przestac to niech wpisze STOP
+#albo nic, po czym program musi podac sume liczb parzystych podanych przez uzytkownika jak i nieparzystych,
+
+#musze przemycic stringi do tego wszystkiego, polaczyc to z petla ktora, kiedy zostanie zatrzymana, policzy ile wyszlo liczby parzystych a ile nieparzystych
+
 number = int(input("Podaj liczbę: "))
 print("Liczba to: " + str(number))
 
