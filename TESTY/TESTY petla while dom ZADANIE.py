3e74ec9084d1ebb82dd2c5f215572a59f1019d0a
WJASK90 <jaskot.w92@gmail.com>
2024-12-02 20:58:23 +0100

    testy

--- TESTY/TESTY petla while dom ZADANIE.py
+++ TESTY/TESTY petla while dom ZADANIE.py
@@ -26,13 +26,55 @@
 #     else int(liczba) == "":
 #         print("Koniec pobierania liczb")
 
-while True:
-    liczba = int(input("Podaj liczbę lub naciśnij Enter aby zakończyć: "))
-    int(liczba)
-    if int(liczba) == "":
-        break
-    if int(liczba) == liczba % 2 == 0:
-        print("To jest liczba parzysta")
-    else:
-        print("To jest liczba nieparzysta")
 
+# BY CHATGPT
+# Inicjalizacja zmiennych do sumowania liczb parzystych i nieparzystych
+# suma_parzystych = 0
+# suma_nieparzystych = 0
+#
+# while True:
+#     # Pobieranie liczby od użytkownika
+#     liczba = input("Podaj liczbę całkowitą (lub naciśnij Enter, aby zakończyć): ")
+#
+#     # Jeśli użytkownik zostawi puste pole, czyli nie poda liczby, konczymy
+#     if liczba == "":
+#         break
+#
+#     # Próbujemy przekonwertować dane wejściowe na liczbę całkowitą
+#     try:
+#         liczba = int(liczba)
+#     except ValueError:
+#         print("To nie jest liczba całkowita, spróbuj ponownie.")
+#         continue
+#
+#     # Dodawanie liczby do sumy parzystej lub nieparzystej
+#     if liczba % 2 == 0:
+#         suma_parzystych += liczba
+#     else:
+#         suma_nieparzystych += liczba
+#
+# # Wyświetlenie sum
+# print(f"Suma liczb parzystych: {suma_parzystych}")
+# print(f"Suma liczb nieparzystych: {suma_nieparzystych}")
+
+suma_parzystych = 0
+suma_nieparzystych = 0
+liczby = [] #lista do przechowywania liczb
+
+while True:
+    # Pobieranie liczby od użytkownika
+    print("Cześć! Podaj liczbę całkowitą, lub naciśniej Enter aby zakończyć")
+    liczba = input("Twoja liczba: ")
+    if liczba == "":
+        break
+
+    liczba = int(liczba)
+
+    # Jeśli użytkownik zostawi puste pole, czyli nie poda liczby, konczymy
+    if liczba % 2 == 0:
+        suma_parzystych += liczba
+    else:
+        suma_nieparzystych += liczba
+
+print("Suma liczb parzystych: " + str(suma_parzystych))
+print("Suma liczb nieparzystych: " + str(suma_nieparzystych))
--- TESTY/Zad. domowe01 z 09-10.11.24.py
+++ TESTY/Zad. domowe01 z 09-10.11.24.py
@@ -3,31 +3,24 @@
 # • niepodanie liczby powinno zakończyć wprowadzanie danych,
 # • program powinien podać sumę liczb parzystych oraz sumę liczb nieparzystych.
 
-# Inicjalizacja zmiennych do sumowania liczb parzystych i nieparzystych
 suma_parzystych = 0
 suma_nieparzystych = 0
+liczby = [] #lista do przechowywania liczb
 
 while True:
     # Pobieranie liczby od użytkownika
-    liczba = input("Podaj liczbę całkowitą (lub naciśnij Enter, aby zakończyć): ")
-
-    # Jeśli użytkownik zostawi puste pole, czyli nie poda liczby, konczymy
+    print("Cześć! Podaj liczbę całkowitą, lub naciśniej Enter aby zakończyć")
+    liczba = input("Twoja liczba: ")
     if liczba == "":
         break
 
-    # Próbujemy przekonwertować dane wejściowe na liczbę całkowitą
-    try:
-        liczba = int(liczba)
-    except ValueError:
-        print("To nie jest liczba całkowita, spróbuj ponownie.")
-        continue
+    liczba = int(liczba)
 
-    # Dodawanie liczby do sumy parzystej lub nieparzystej
+    # Jeśli użytkownik zostawi puste pole, czyli nie poda liczby, konczymy
     if liczba % 2 == 0:
         suma_parzystych += liczba
     else:
         suma_nieparzystych += liczba
 
-# Wyświetlenie sum
-print(f"Suma liczb parzystych: {suma_parzystych}")
-print(f"Suma liczb nieparzystych: {suma_nieparzystych}")
+print("Suma liczb parzystych: " + str(suma_parzystych))
+print("Suma liczb nieparzystych: " + str(suma_nieparzystych))
