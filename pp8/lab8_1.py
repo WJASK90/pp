e0ee91c57c7e933adaa4a1e42e18f2b54472b563
WJASK90 <jaskot.w92@gmail.com>
2024-12-04 21:26:30 +0100

    testy

--- pp8/lab8_1.py
+++ pp8/lab8_1.py
@@ -1,9 +1,9 @@
 #technika wykonywania cwiczen: rozdziel na etapy, dodawaj coraz bardziej skomplikowane kroki
 #Wyświetl liczby od 1 do 100 z pominięciem liczb podzielnych przez 3
 #zastosowałem modulo, używając // dzieliło wartości
-for i in range(0, 100):
-    if (i % 3):
-        print(i, end=" ")
+# for i in range(0, 100):
+#     if (i % 3):
+#         print(i, end=" ")
 
 
 #for i in range(1,101):
