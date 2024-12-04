357e8c391469d8b50095003034f5096d8ef44b1d
WJASK90 <jaskot.w92@gmail.com>
2024-12-04 18:53:42 +0100

    testy

--- TESTY/TESTY różne 2.py
+++ TESTY/TESTY różne 2.py
@@ -6,3 +6,21 @@ print(message)
 
 message = 'Witaj, świecie książki "Python. Instrukcje dla programisty"!'
 print(message)
+
+for i in range(3):
+    print(i < 3)
+
+for i in range(3):
+    print("First")
+    print("Second")
+
+password = "SecretWord"
+guess1 = "1234"
+print(guess1 != password)
+
+password = "SecretWord"
+guess = input()
+while guess != password:
+    guess = input()
+print("Access Granted")
+
