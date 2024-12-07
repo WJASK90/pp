d1aa452a6324c7cec5b9818acec76bfce263ce99
WJASK90 <jaskot.w92@gmail.com>
2024-12-07 15:10:48 +0100

    testy

---
+++ pp13/demo13 change_value.py
@@ -0,0 +1,26 @@
+# def change_value(n): #tu bierzemy jakby kopie tej wartosci
+#     print("przed zmianą:", n)
+#     n += 1
+#     print("po zmianie" , n)
+#
+# value = 7
+# change_value(value)
+# print(value) #wyswietli sie 7, bo to n traktujemy jako zmienna lokalna
+
+# wynik:
+# przed zmianą: 7
+# po zmianie 8
+# 7
+
+def change_value(my_list_1): #tu bierzemy jakby kopie tej wartosci
+    my_list_1 = [0, 0]
+
+
+my_list_2 = [1, 2]
+change_value(my_list_2)
+print(my_list_2)
+
+#czemu wynik to [1, 2]?
+
+# my_list_2 = [1, 2] to specjalna wartosc zmienna. Why? Bo jak wchodzimy do funkcji change_value, nie robimy kopii listy w nawiasie, robimy kopie referencji
+# do tej listy, ale nie listy.
