1e71491e86cf83d31afd1b4ad815833f63a8ec3a
WJASK90 <jaskot.w92@gmail.com>
2024-12-07 12:22:21 +0100

    testy

--- pp12/lab 12 test.py
+++ pp12/lab 12 test.py
@@ -1,3 +1,16 @@
 # 1. Napisz funkcję, której zadaniem będzie wyświetlić na ekranie dowolny znak, dowolną
 # ilość razy, w poziomie lub w pionie.
 
+#funkcja aby wywolywac gwiazdki *
+
+sign_choice = input("Wpisz znak: ")
+multiple = int(input("Ile razy wyświetlić znak? " ))
+# vert =
+# hor =
+
+def show_sign(how_many): #dodajemy how_many czyli ile razy wyswietlic
+    print(sign_choice * multiple)
+
+show_sign(5)
+show_sign(8)
+show_sign(1)
