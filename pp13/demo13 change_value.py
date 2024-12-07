8b556f3bb3232f1469dc2818068533118edb39d9
WJASK90 <jaskot.w92@gmail.com>
2024-12-07 15:20:50 +0100

    testy

--- pp13/demo13 change_value.py
+++ pp13/demo13 change_value.py
@@ -12,15 +12,23 @@
 # po zmianie 8
 # 7
 
-def change_value(my_list_1): #tu bierzemy jakby kopie tej wartosci
-    my_list_1 = [0, 0]
-
-
-my_list_2 = [1, 2]
-change_value(my_list_2)
-print(my_list_2)
+# def change_value(my_list_1): #tu bierzemy jakby kopie tej wartosci
+#     my_list_1 = [0, 0]
+#
+#
+# my_list_2 = [1, 2]
+# change_value(my_list_2)
+# print(my_list_2)
 
 #czemu wynik to [1, 2]?
 
 # my_list_2 = [1, 2] to specjalna wartosc zmienna. Why? Bo jak wchodzimy do funkcji change_value, nie robimy kopii listy w nawiasie, robimy kopie referencji
 # do tej listy, ale nie listy.
+
+def change_value(my_list_1):
+    my_list_1[0] = 999 #indeks 0 to pierwsza liczba, wiec wynik bedzie [999, 2]
+
+
+my_list_2 = [1, 2]
+change_value(my_list_2)
+print(my_list_2)
