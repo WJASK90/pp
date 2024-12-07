ddbcf45e09337fa9871639e70015e94df0658ddf
WJASK90 <jaskot.w92@gmail.com>
2024-12-07 08:38:46 +0100

    testy

--- pp11/lab 11 testy 2.py
+++ pp11/lab 11 testy 2.py
@@ -1,3 +1,4 @@
 # 2. Napisz skrypt pobierający od użytkownika serię liczb całkowitych, a
 # następnie wyświetl je w kolejności malejącej pozbywając się wcześniej
 # duplikatów.
+
--- pp11/lab 11 testy 3.py
+++ pp11/lab 11 testy 3.py
@@ -2,3 +2,4 @@
 # • stwórz wirtualną szachownicę,
 # • na wirtualnej szachownicy rozmieść losowo 2. dowolne figury szachowe i 3. piony,
 # • zaprezentuj użytkownikowi stan wirtualnej szachownicy.
+
