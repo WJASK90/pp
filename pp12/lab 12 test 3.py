3c0a883b46e4deb77429cbf570f686da378f9e78
WJASK90 <jaskot.w92@gmail.com>
2024-12-07 12:11:42 +0100

    testy

---
+++ pp12/lab 12 test 3.py
@@ -0,0 +1,7 @@
+# 3. Napisz program obliczającej wskaźnik BMI (Body Mass Index), w tym celu:
+# • zapytaj użytkownika o wzrost i wagę,
+# • stwórz funkcję obliczającą wskaźnik BMI na podstawie podanych przez użytkownika danych,
+# • stwórz funkcję wyznaczającą odpowiednią kategorię (niedowaga, waga prawidłowa, nadwaga,
+# otyłość) na podstawie wskaźnika BMI,
+# • zaprezentuj wyniki korzystając z wcześniej przygotowanych funkcji.
+
---
+++ pp12/lab 12 test.py
@@ -0,0 +1,3 @@
+# 1. Napisz funkcję, której zadaniem będzie wyświetlić na ekranie dowolny znak, dowolną
+# ilość razy, w poziomie lub w pionie.
+
