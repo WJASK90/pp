fef10c36ab1756898570111cab8483d7930ccc9c
WJASK90 <jaskot.w92@gmail.com>
2024-12-08 17:23:47 +0100

    testy

---
+++ pp15/lab 15 2.py
@@ -0,0 +1,21 @@
+# 2. Napisz program pobierający od użytkownika 3 liczby zmiennoprzecinkowe.
+# Uwzględnij możliwość pomyłki użytkownika.
+
+
+numbers = []
+counter = 1
+
+while True:
+    if counter > 3:
+        break
+    try: #tutaj badamy sytuacje wyjatkowo, pobieramy liczbe, zapisujemy zmiennia i pobieramy input typo float
+        number = float(input("Podaj {} liczbę zmiennoprzecinkową: ".format(counter)))
+        numbers.append(number)
+        counter += 1
+    except:
+        print("Podana wartość jest niepoprawna, spróbuj ponownie!")
+
+print(numbers)
+
+
+#1
