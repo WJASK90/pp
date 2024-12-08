e5d441258145832e96cb493f3424b6febefb6662
WJASK90 <jaskot.w92@gmail.com>
2024-12-08 08:49:12 +0100

    testy

--- pp13/lab 13 test1.py
+++ pp13/lab 13 test1.py
@@ -1,2 +1,16 @@
 # 1. Napisz funkcję podnoszącą do wskazanej potęgi wszystkie elementy wskazanej listy.
 
+lista_1 = [1,2,3,4,5,6,7,8,9,10]
+
+def do_potegi(lista_1):
+    for i in lista_1:
+        print(i ** 2)
+
+
+do_potegi(lista_1)
+
+
+
+
+# for i in lista_1:
+#     print(i ** 2)
