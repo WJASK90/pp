dfc362a40ac5ae0bec8344849e1571db5d2c4043
WJASK90 <jaskot.w92@gmail.com>
2024-12-08 16:04:13 +0100

    testy

--- pp16/demo 16 4 platform.py
+++ pp16/demo 16 4 platform.py
@@ -9,4 +9,4 @@ print(platform.system())
 print(platform.version())
 print()
 print(platform.python_implementation()) #na jakiej preferencyjnej implementacji pythonia dzialamy
-print(platform.python_version_tuple()) #poda werjse w krotce
+print(platform.python_version_tuple()) #poda wersje w krotce/tuple
