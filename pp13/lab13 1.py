24833b664e86aa9581076c53e10837fff764e620
WJASK90 <jaskot.w92@gmail.com>
2024-12-08 09:19:16 +0100

    testy

--- pp13/lab13 1.py
+++ pp13/lab13 1.py
@@ -1,1 +1,13 @@
 # 1. Napisz funkcję podnoszącą do wskazanej potęgi wszystkie elementy wskazanej listy.
+
+def pow(numbers, exponent): #funkcja przyjmuje liste numbers i drugi parametr do jakiej liczby potegi beda podnoszone
+    numbers = numbers[:] # zabezpieczenie przed modyfikacja listy, jeśli nie to by nachodziły na siebie wartości (przykład na tablicy, nagranie 07.12)
+    for i in range(len(numbers)):
+        numbers[i] = numbers[i] ** exponent
+    return numbers
+
+
+numbers = [1, 2, 3]
+print(pow(numbers, 2))
+
+print(pow(numbers, 5))
