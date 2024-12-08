66102617aed04a32f2d085c4a87994f57fd51f5e
WJASK90 <jaskot.w92@gmail.com>
2024-12-08 11:15:25 +0100

    testy

---
+++ pp14/student_marks14.py
@@ -0,0 +1,15 @@
+# Elektroniczny dziennik z ocenami studentów
+# - wprowadzanie ocen
+# - wyswietlanie ocen wraz z wprowadzoną średnią
+
+#NAJLEPIEJ UZYC SLOWNIK GDZIE BEDZIEMY MIEC SPIS STUDENTOW, nie krotka lub lista bo trudno sie do tego odwołać
+
+# students = {"Tomek": [2, 3, 4], "Agata": [5, 5, 5]} # w strukturze beda wartosci ktore beda LISTAMI
+
+def get_data(): #funkcja do pobierania danych
+    pass
+
+def print_summary(students): #podsumowanie
+    pass
+
+print_summary(get_data()) #pobierz podsumowanie pobranych danych od uzytkownika
