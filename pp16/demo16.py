0c4ba7f0afa733dc7484be167723a4ea1e421611
WJASK90 <jaskot.w92@gmail.com>
2024-12-08 15:41:27 +0100

    testy

---
+++ pp16/demo16.py
@@ -0,0 +1,9 @@
+import math #importujemy modul math, tak jak to robilismy z modulem random
+import sys
+
+
+# print(math.pi)
+# print(math.sin(math.pi / 2))
+
+print(math.factorial(5)) #silnia
+print(math.floor(4.99999999))
