838f73513a0479d2308b56107d650b84e05ea235
WJASK90 <jaskot.w92@gmail.com>
2024-12-08 08:51:12 +0100

    testy

--- pp13/lab 13 test1.py
+++ pp13/lab 13 test1.py
@@ -1,10 +1,11 @@
 # 1. Napisz funkcję podnoszącą do wskazanej potęgi wszystkie elementy wskazanej listy.
 
 lista_1 = [1,2,3,4,5,6,7,8,9,10]
+do_ktorej_p = int(input("Do której potęgi podnosimy liczby? Wpisz numer: "))
 
 def do_potegi(lista_1):
     for i in lista_1:
-        print(i ** 2)
+        print(i ** do_ktorej_p)
 
 
 do_potegi(lista_1)
