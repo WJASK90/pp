8d1ce91c66b3e87a2ee20261b6cb44753781544d
WJASK90 <jaskot.w92@gmail.com>
2024-12-08 10:52:53 +0100

    testy

---
+++ pp14/demo14 slowniki.py
@@ -0,0 +1,19 @@
+#klasyczny slownik to ksiazka telefoniczna, dobry przyklad
+
+phones = {"Tomek": 555555, "Ada" : 123123123, "Karol": 444333222} # klucz:wartość
+print(phones)
+
+
+phones1 = {"Tomek": 555555, "Ada" : 123123123, "Karol": 444333222, "Tomek": 99999} # klucz:wartość
+print(phones1) #te same klucze nadpisuja wartosc jesli jest inna!
+
+#stworzmy slownik jezyka angielskiego
+animals_dict = {
+    "kot": "cat",
+    "pies": "dog",
+    "chomik": "hamster",
+}
+
+print(animals_dict["kot"]) #tutaj tez mozemy sie odwolac to danego elementu, ale wpisujemy KLUCZ a nie NUMER INDEKSU
+print(animals_dict.get("kot")) #rowniez dostajemy KLUCZ.
+#roznica miedzy [] a .get jest taka ze jesli w pierwszym print wpiszemy nieistniejacy klucz, to bedzie ERROR, a .get nie da bledu tylko None
