cf006ed8c2d6fbc4a955f39ae77e7c348c724d1a
WJASK90 <jaskot.w92@gmail.com>
2024-12-08 09:33:46 +0100

    testy

--- pp13/lab13 2.py
+++ pp13/lab13 2.py
@@ -1,4 +1,21 @@
 #REKURENCJA - Tabliczka mnozenia
 
+#rekurencja = mechanizmy ktory wywoluje sam siebie
+
 # 2. Korzystając z rekurencji wypisz na ekranie tabliczkę mnożenie do 100.
+
+# 1 x 1 = 1
+
+def show_operation(a, b):
+    print(a, "x", b, "=", a*b) #graficznie a x b = a*b
+    if a == 10 and b == 10:
+        return
+    elif a == 10:
+       show_operation(a, b + 1)
+    else:
+        show_operation(a + 1, b)
+
+#wchodzimy z dwoma liczbami, sprawdza warunek czy czasem a i b nie maja wartosci 10, jak obydwa osiagna 10 to zatrzymujemy operacje
+
+show_operation(1, 1)
 
