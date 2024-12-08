9a10b9f06d23c6780108f0f9c65b40dd198cc6ae
WJASK90 <jaskot.w92@gmail.com>
2024-12-08 12:06:28 +0100

    testy

--- pp14/lab 14 test 1.py
+++ pp14/lab 14 test 1.py
@@ -3,3 +3,13 @@
 # • zapytaj użytkownika o imię,
 # • wyświetl użytkownikowi numer telefonu lub informację o jego braku.
 
+#klasyczny slownik to ksiazka telefoniczna, dobry przyklad --> slowniki sa ZMIENNE
+
+phones_1 = {"Tomasz": 39555, "Agata": 39666, "Karol": 39777, "Cezary": 39888} # klucz:wartość
+
+select_name = input("Potrzebujesz numeru telefono do kogo? Podaj imię z Tomasz/Agata/Karol/Cezary: ")
+for i in phones_1:
+    if i in phones_1.keys():
+        print(phones_1.get(select_name, "Brak takiej osoby w książce telefonicznej"))
+        break
+
