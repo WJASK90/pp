bbba4fa6bd77bccd74dbc3597f3e4f6e55e1abb7
WJASK90 <jaskot.w92@gmail.com>
2024-12-08 15:02:57 +0100

    testy

--- pp15/lab 15 testy 2.py
+++ pp15/lab 15 testy 2.py
@@ -1,4 +1,8 @@
 # 2. Napisz program pobierający od użytkownika 3 liczby zmiennoprzecinkowe.
 # Uwzględnij możliwość pomyłki użytkownika.
 
+pobieranie = float(input("Podaj liczbę zmiennoprecinkowe (możesz wpisać jedną liczbę): "))
+
+
+
 
--- pp15/lab15 1.py
+++ pp15/lab15 1.py
@@ -4,3 +4,12 @@
 # powinna zwrócić None.
 
 
+def safe_int(arg):
+    try:
+        return int(arg)
+    except:
+        return None
+
+print(safe_int(1))
+print(safe_int(7.2))
+print(safe_int("jeden"))
