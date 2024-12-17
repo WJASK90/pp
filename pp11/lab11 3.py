89a0efbce2b0f196e9234f39649405d082b2c1e3
WJASK90 <jaskot.w92@gmail.com>
2024-12-17 19:05:59 +0100

    testy

--- pp11/lab11 3.py
+++ pp11/lab11 3.py
@@ -14,7 +14,7 @@ counter = 0 #dajemy counter bo potrzebujemy petle WHILE, WHILE poniewaz mamy te 
 #petla sie obraca dopoki counter bedzie mniejsze od 5 - czyli trzeba dokonac 5 rozstawien
 
 counter = 0
-while counter < 5:
+while counter < len(pieces):
     row = random.randint(0, 7) #linie, mamy 8 pol, indeks zaczyna sie od 0, wiec od 0 do 7
     col = random.randint(0, 7) #kolumny
     if chessboard[row][col] == BLANK_SQUARE: #jezeli jest puste pole, to wstawiaj
@@ -24,8 +24,8 @@ while counter < 5:
 #wyswietlamy liste listy, czyli petla w petli
 for row in range(len(chessboard)): #sprawdzamy ile ma elementow szachownica/lista chessboard
     if row == 0:
-        print( " ","A","B","C","D","E","F","G","H", sep="  ") #aby uzyskac pozycje graficzna pionkow na szachownicy
-        print(row + 1, end=" ") #aby uzyskac pozycje graficzna pionkow na szachownicy
+        print(" ","A","B","C","D","E","F","G","H", sep="  ") #aby uzyskac pozycje graficzna pionkow na szachownicy
+    print(row + 1, end=" ") #aby uzyskac pozycje graficzna pionkow na szachownicy
     for col in range(len(chessboard[row])): #sprawdzamy ile elementow ma konkretny wiersz
         print(chessboard[row][col], end=" ") #end=" " bo wtedy wyswietlamy po kolei pola
     print() #konczymy
