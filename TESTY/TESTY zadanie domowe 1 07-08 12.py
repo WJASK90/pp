146771c45408d95a1e25bc362d0809d9de33c1ea
WJASK90 <jaskot.w92@gmail.com>
2025-01-15 20:55:22 +0100

    testy

--- TESTY/TESTY zadanie domowe 1 07-08 12.py
+++ TESTY/TESTY zadanie domowe 1 07-08 12.py
@@ -8,3 +8,18 @@ def remove_duplicates(list_1):
 
 list_2 = [1, 1, 2, 3, 4, 4, 5, 6, 6, 6, 7, 7, 8]
 print(remove_duplicates(list_2))
+
+
+
+
+def remove_duplicates_2(list):
+    unique_list = []  # Tworzymy nową listę
+    for item in list:  # Przechodzimy przez wszystkie elementy w liście
+        if item not in unique_list:  # Sprawdzamy, czy element już się pojawił
+            unique_list.append(item)  # Jeśli nie, dodajemy go do nowej listy
+    return unique_list  # Zwracamy listę bez duplikatów
+
+# Przykład użycia:
+original_list = [1, 2, 3, 2, 4, 5, 3, 6, 1]
+result = remove_duplicates_2(original_list)
+print(result)  # Wynik: [1, 2, 3, 4, 5, 6]
