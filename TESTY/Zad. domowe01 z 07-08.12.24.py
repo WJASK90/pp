496628c2eee26af0d9067392dd1aff5a4449f6f4
WJASK90 <jaskot.w92@gmail.com>
2025-01-16 21:16:27 +0100

    testy

--- TESTY/Zad. domowe01 z 07-08.12.24.py
+++ TESTY/Zad. domowe01 z 07-08.12.24.py
@@ -1,11 +1,11 @@
 # 1. Napisz funkcję, która pozbędzie się z listy powtarzających się elementów.
 
-def smierc_duplikatom(list):
-    bez_duplikatow = []
+def death_to_duplicates(list):
+    no_duplicates = []
     for i in list:
-        if i not in bez_duplikatow:
-            bez_duplikatow.append(i)
-    return bez_duplikatow
+        if i not in no_duplicates: #jeśli elementu nie ma w liście to zostaje dodany do no_duplicates, w ten sposób nie ma duplikatów
+            no_duplicates.append(i)
+    return no_duplicates #można by przemienić liste na Set/Zbiór ale na zajęciach ich nie mieliśmy, widziałem je w aplikacji Sololearn wtedy duplikaty są ignorowane przy wywoływaniu
 
-lista_przykladowa = [1, 1, 2, 3, 4, 4, 5, 6, 6, 6]
-print(smierc_duplikatom(lista_przykladowa))
+example_list = [1, 1, 2, 3, 4, 4, 5, 6, 6, 6]
+print(death_to_duplicates(example_list))
