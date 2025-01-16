a458437c58a204d8b6dd09f646e9a604f56a2423
WJASK90 <jaskot.w92@gmail.com>
2025-01-16 21:17:03 +0100

    testy

--- TESTY/Zad. domowe01 z 07-08.12.24.py
+++ TESTY/Zad. domowe01 z 07-08.12.24.py
@@ -5,7 +5,7 @@ def death_to_duplicates(list):
     for i in list:
         if i not in no_duplicates: #jeśli elementu nie ma w liście to zostaje dodany do no_duplicates, w ten sposób nie ma duplikatów
             no_duplicates.append(i)
-    return no_duplicates #można by przemienić liste na Set/Zbiór ale na zajęciach ich nie mieliśmy, widziałem je w aplikacji Sololearn wtedy duplikaty są ignorowane przy wywoływaniu
+    return no_duplicates #można by przemienić liste na Set/Zbiór ale na zajęciach ich nie mieliśmy, widziałem je w aplikacji Sololearn, wtedy duplikaty są ignorowane przy wywoływaniu
 
 example_list = [1, 1, 2, 3, 4, 4, 5, 6, 6, 6]
 print(death_to_duplicates(example_list))
