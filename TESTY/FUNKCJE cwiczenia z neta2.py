22e1d4082912d07dfbe16636d673b11b682290d1
WJASK90 <jaskot.w92@gmail.com>
2025-01-17 21:14:26 +0100

    testy

---
+++ TESTY/FUNKCJE cwiczenia z neta2.py
@@ -0,0 +1,37 @@
+# Ćwiczenie 1: Funkcja obliczająca silnię
+# Napisz funkcję silnia(n), która zwróci silnię liczby n. Pamiętaj, że
+# silnia to iloczyn wszystkich liczb całkowitych od 1 do n.
+# Przykład:
+
+# print(silnia(5))  # Wynik: 120
+
+def silnia(i):
+
+
+
+
+# Napisz funkcję czy_pierwsza(liczba), która sprawdzi,
+# czy podana liczba jest liczbą pierwszą. Liczba pierwsza to taka,
+# która dzieli się tylko przez 1 i samą siebie.
+# Przykład:
+# print(czy_pierwsza(7))  # Wynik: True
+# print(czy_pierwsza(10)) # Wynik: False
+
+# def czy_pierwsza(i):
+#     for i in range(i+1):
+#         if i == i and i // i == i:
+#             if i != int:
+#                 print("Nie!")
+#             return i
+#
+# # counter = 1
+#
+# print(czy_pierwsza(10))
+
+
+
+
+
+
+
+
