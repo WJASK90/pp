96a85ef2fe9405dfce4a4e9f262be73565ccfcbd
WJASK90 <jaskot.w92@gmail.com>
2025-01-17 23:20:18 +0100

    testy

--- TEST_2/lab14-2-testy.py
+++ TEST_2/lab14-2-testy.py
@@ -1,13 +1,13 @@
 # 2. Napisz funkcję zamieniającą 3 listy na 1 krotkę bez powtarzających się elementów.
 # Przykład: [1, 2], [1, 1], [4, 4, 4] -> (1, 2, 4)
 
-def merge_list_without_duplicate(source, target):
-    for element in source:
-        if element not in target:
+def merge_list_without_duplicate(source, target): #SOURCE to lista zrodlowa a TARGET to lista docelowa
+    for element in source: #przechodzimy po elementach listy zrodlowej!
+        if element not in target: #jak elementu nie ma, to go dodajemy
             target.append(element)
 
 def transform_data(list1, list2, list3):
-    result = []
+    result = [] #lista, mutowalna!
     merge_list_without_duplicate(list1, result)
     merge_list_without_duplicate(list2, result)
     merge_list_without_duplicate(list3, result)
