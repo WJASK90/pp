18bbcc67ce94d5e4dda85094ba749d594b2f1e17
WJASK90 <jaskot.w92@gmail.com>
2025-01-17 22:24:18 +0100

    testy

--- TEST_2/lab13-2-testy.py
+++ TEST_2/lab13-2-testy.py
@@ -4,7 +4,7 @@ def multiple_table(a, b):
     print(a, "X", b , "=", a*b)
     if a == 10 and b == 10: #sprawdzamy czy a i b nie osiagnely wartosci 10
         return
-    elif a == 10: #sprawdz czy b to 10 i tez wyjdz
+    elif a == 10: #sprawdz czy a to 10 i tez wyjdz
         multiple_table(1, b +1) #jesli a osiaga 10 to a sie resetuje i liczy od 1 a b leci dalej
     else:
         multiple_table(a + 1, b)
