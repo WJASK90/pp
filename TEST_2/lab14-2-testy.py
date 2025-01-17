3eb9723dffc0b220e5ce747703e3111e99df9584
WJASK90 <jaskot.w92@gmail.com>
2025-01-17 23:00:17 +0100

    testy

--- TEST_2/lab14-2-testy.py
+++ TEST_2/lab14-2-testy.py
@@ -1,27 +1,18 @@
 # 2. Napisz funkcję zamieniającą 3 listy na 1 krotkę bez powtarzających się elementów.
 # Przykład: [1, 2], [1, 1], [4, 4, 4] -> (1, 2, 4)
 
+def merge_list_without_duplicate(source, target):
+    for element in source:
+        if element not in target:
+            target.append(element)
 
-lista_1 = [1, 1, 3]
-lista_2 = [4, 5, 5]
-lista_3 = [7, 7, 7]
+def transform_data(list1, list2, list3):
+    result = []
+    merge_list_without_duplicate(list1, result)
+    merge_list_without_duplicate(list2, result)
+    merge_list_without_duplicate(list3, result)
+    return tuple(result) #podczas zwracania konwertujemy w zwrotke/tuple
 
-def pierwsza_lista(lista_1):
-    for i in range(len(lista_1)):
-        if i != 1:
-            print(lista_1[i])
-            lista_1 = tuple
-def druga_lista(lista_2):
-    for i in range(len(lista_2)):
-        if i != 5:
-            print(lista_2[i])
-            lista_2 = tuple
-def trzecia_lista(lista_3):
-    for i in range(len(lista_3)):
-        if i != 7:
-            print(lista_3[i])
-            lista_3 = tuple
-
-pierwsza_lista(lista_1)
-druga_lista(lista_2)
-trzecia_lista(lista_3)
+print(transform_data(list1=[1, 2], list2=[1, 1], list3=[4, 4, 4]))
+print(transform_data(list1=[1, 2, 3], list2=[1, 2, 3], list3=[4, 5, 6]))
+print(transform_data(list1=["Ala" , "ma"], list2=["Ala", "ma", "kota"], list3=["Mysz"]))
