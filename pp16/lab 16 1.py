29292c8ae92764e7682d2416a11b9546728b56d2
WJASK90 <jaskot.w92@gmail.com>
2025-01-18 11:16:08 +0100

    testy

--- pp16/lab 16 1.py
+++ pp16/lab 16 1.py
@@ -9,21 +9,14 @@ from random import sample
 import math
 
 #info o procesorze
-print(platform.processor())
+print("Procesor:", platform.processor())
 
 #losujemy 3 niepowtarzalne liczby ze zbioru 1-10)
-def range_of_numbers():
-    number = [i for i in range (1,11)]
-    three_numbers = sample(number, k=3)
-    return three_numbers
 
-print(range_of_numbers())
+print("Losowanie:", random.sample(range(1,11), k=3))
 
 #wyznaczam sinus 90 stopni
 
-angle = math.radians(90)
-sinus = math.sin(angle)
-
-print(sinus)
+print("Sinus 90 stopni:", math.sin(math.radians(90)))
 
 
