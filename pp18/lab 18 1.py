1b13169ff56c602f08453db6aea6eb70d56790e2
WJASK90 <jaskot.w92@gmail.com>
2025-01-18 16:50:35 +0100

    testy

--- pp18/lab 18 1.py
+++ pp18/lab 18 1.py
@@ -2,12 +2,7 @@
 # wraz z punktami kodowymi dla każdej litery.
 
 # Polski alfabet (małe litery z diakrytycznymi znakami)
-polski_alfabet = [
-    'a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-    'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'q', 'r', 's', 'ś', 't', 'u', 'v', 'w',
-    'x', 'y', 'z', 'ź', 'ż'
-]
+polski_alfabet = [aąbcćdeęfghijklłmnoópqrsśtuvwxyzźżz]
 
-# Wyświetlenie liter alfabetu oraz ich punktów kodowych
 for litera in polski_alfabet:
-    print(litera + ": " + str(ord(litera)))
+    print(litera, ord(litera))
--- pp18/lab 18 2.py
+++ pp18/lab 18 2.py
@@ -3,3 +3,17 @@
 # Na przykład dla ciągu "abracadabra" funkcja powinna zwrócić słownik { 'a': 5, 'b':
 # 2, 'r': 2, 'c': 1, 'd': 1 }.
 
+txt = """W Pacanowie kozy kują
+    więc Koziołek, mądra głowa,
+    błąka się po całym świecie,
+    aby dojść do Pacanowa."""
+
+def count_letters(string):
+    stats = {}
+    for letter in string:
+        stats[letter] = stats.get(letter, 0) + 1
+    return stats
+
+print(count_letters('abc'))
+print(count_letters('Ala ma kota.'))
+print(count_letters(txt))
