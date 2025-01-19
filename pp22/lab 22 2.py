77e7b69d0fb6e83d14682afa500a98c983477002
WJASK90 <jaskot.w92@gmail.com>
2025-01-19 13:04:43 +0100

    testy

--- pp22/lab 22 2.py
+++ pp22/lab 22 2.py
@@ -1,4 +1,15 @@
 # 2. Napisz klasę zliczającą wszystkie powstałe swoje instancje
 
-class Count_instances():
-    pass
+class C:
+    counter = 0
+
+    def __init__(self):
+        C.counter += 1
+
+    def get_counter(self):
+        return C.counter
+
+for _ in range(100):
+    c = C()
+
+print(c.get_counter())
--- pp22/lab 22 3.py
+++ pp22/lab 22 3.py
@@ -4,4 +4,23 @@
 # • zademonstruj działanie wybranych metod na przykładowych egzemplarzach.
 
 class Book:
-    pass
+    def __init__(self, title, author, publisher, year):
+        self.__title = title
+        self.__author = author
+        self.__publisher = publisher
+        self.__year = year
+
+    def show_short_info(self):
+        print(f"tytuł {self.__title} autor: {self.__author}") #piszemy z f-string czyli f""
+
+    def show_full_info(self):
+        print(f"tytuł {self.__title} autor: {self.__author} wydawca: {self.__publisher} rok wydania: {self.__year}")
+
+books = []
+books.append(Book("Dzieci z Bullerbyn", author="Astrid Lindgren", publisher="Nasza Księgarnia", year=2014))
+books.append(Book("Moby Dick", author="Herman Marville", publisher="Amercon", year=2009))
+books.append(Book("Python. Wprowadzenie", author="Mark Lutz", publisher="Helion", year=2022))
+#wymusza wszystkie pola ALE jak dasz wartosc domyslne (jak w funkcjach) to mozesz to obejsc :)
+for book in books:
+    book.show_full_info()
+
