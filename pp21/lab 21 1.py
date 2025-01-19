a5317f3ce4c9ddf3b2b3fee8521678d36571ca14
WJASK90 <jaskot.w92@gmail.com>
2025-01-19 10:37:10 +0100

    testy

--- pp21/lab 21 1.py
+++ pp21/lab 21 1.py
@@ -1,1 +1,7 @@
 # 1. Stwórz klasę o nazwie Student i utwórz 3 instancje nowo utworzonej klasy.
+
+# (Po zdefiniowaniu klasy, należy utworzyć jej instancje. Instancja klasy to obiekt, który jest konkretną realizacją tej klasy. )
+
+class Student:
+    pass
+
