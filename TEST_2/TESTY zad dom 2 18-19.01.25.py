9c5cc3a8230a5bdb26f21e88defa4c6fc4394be1
WJASK90 <jaskot.w92@gmail.com>
2025-02-11 19:30:33 +0100

    testy

--- TEST_2/TESTY zad dom 2 18-19.01.25.py
+++ TEST_2/TESTY zad dom 2 18-19.01.25.py
@@ -3,14 +3,7 @@
 # •pobierz od użytkownika szukane słowo,
 # •wyświetl liczbę wystąpień zliczanego słowa.
 
-# file_path = C:\Users\Wojtek-PC\Desktop\Czarny Kot Edgar Allan Poe
-#
-# print(input("Wpisz słowo - sprawdzimy, ile razy występuje w pliku tekstowym: "))
-#
-# def count_words(word):
-#     words_list = []
-#     for i in word:
-#         words_list.append(i)
+
 
 class WordCounter:
     def __init__(self, file_path):
@@ -18,17 +11,17 @@ class WordCounter:
         self.text = self.read_file()
 
     def read_file(self):
-        """Wczytuje tekst z pliku"""
+        #pobieram słowo z tekstu
         try:
             with open(self.file_path, 'r', encoding='utf-8') as file:
                 text = file.read()
             return text
-        except FileNotFoundError:
+        except FileNotFoundError: #w wypadku braku wpisanego słowa, zabezpieczamy
             print(f"Błąd: Plik {self.file_path} nie został znaleziony.")
             sys.exit()
 
     def count_word_occurrences(self, word):
-        """Zlicza, ile razy dane słowo występuje w tekście"""
+        #liczym, ile razy dane słowo występuje w tekście
         words = self.text.lower().split()  # dzieli tekst na słowa i ignoruje wielkość liter
         return words.count(word.lower())  # zlicza wystąpienia słowa
 
@@ -46,7 +39,14 @@ occurrences = counter.count_word_occurrences(user_word)
 print(f"To słowo występuje w tekście {occurrences} razy!")
 
 
-
+# file_path = C:\Users\Wojtek-PC\Desktop\Czarny Kot Edgar Allan Poe
+#
+# print(input("Wpisz słowo - sprawdzimy, ile razy występuje w pliku tekstowym: "))
+#
+# def count_words(word):
+#     words_list = []
+#     for i in word:
+#         words_list.append(i)
 
 
 
