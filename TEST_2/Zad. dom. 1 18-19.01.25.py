8389177dbc16354c0ef141bd20cd9a18f780480c
WJASK90 <jaskot.w92@gmail.com>
2025-02-13 20:09:18 +0100

    testy

--- TEST_2/Zad. dom. 1 18-19.01.25.py
+++ TEST_2/Zad. dom. 1 18-19.01.25.py
@@ -1,13 +1,15 @@
 # Napisz funkcję sprawdzającą, czy podany ciąg znaków jest palindromem
 # (czy czyta się tak samo od przodu i od tyłu). Ignoruj wielkość liter oraz spacje.
 
-
+#definiuje funkcję
 def palindrom(sample: str):
     change = sample.lower().replace(" ", "")
     return change == change[::-1]
 
+#lista słów
 samples = ["oko", "łóżko", "zakaz", "Robert", "Ala", "palindrom", "atak kata"]
 
+#sprawdzanie palindromu
 for sample in samples:
     if palindrom(sample):
         print(f'"{sample}" to palindrom :)')
--- TEST_2/Zad. dom. 3 18-19.01.25.py
+++ TEST_2/Zad. dom. 3 18-19.01.25.py
@@ -4,32 +4,6 @@
 # •możliwość grania systemem (6-12 liczb).
 
 
-# from lotto import get_user_numbers, draw_numbers, check_user_numbers #połączone z plikiem lotto.py stamtąd jest zaimportowany moduł! :O
-#
-# print("Witaj w grze LOTTO!")
-# user_numbers = get_user_numbers()
-# print("Pobrane liczby to:", user_numbers)
-#
-# print("\nNaciśnij ENTER, aby dokonać losowania liczb!\n")
-# input()
-# lucky_numbers = draw_numbers()
-# print("Wylosowano liczby:", lucky_numbers)
-# print()
-#
-# result = check_user_numbers(user_numbers, lucky_numbers)
-# if result == 6:
-#     print("GRATULACJE!", "Jesteś milionerem!")
-# elif result == 5:
-#     print("BRAWO!", "Trafiłeś piątkę!", "Zgarniesz sporą sumę!")
-# elif result == 4:
-#     print("Hurra!", "Trafiono czwórkę!", "To całkiem niezły wynik!")
-# elif result == 3:
-#     print("Trafiono trójkę!", "Przysługuję Ci minimalna wygrana!")
-# elif result == 2 or result == 1:
-#     print("Trafiono {} liczbę/y. Było bardzo blisko!".format(result))
-# else:
-#     print("To nie jest Twój dzień!")
-
 from random import sample
 
 class LottoGame:
@@ -41,12 +15,6 @@ class LottoGame:
         if not self.lucky_numbers:
             self.lucky_numbers = sorted(sample(range(1, 50), 6))
         return self.lucky_numbers
-
-    # def draw_numbers(self):
-    #     numbers = list(range(1,50))
-    #     self.lucky_numbers = sample(numbers,6)
-    #     self.lucky_numbers.sort()
-    #     return self.lucky_numbers
 
     def get_game_system(self):
         while True:
