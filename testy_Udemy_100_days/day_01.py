ed39a2c07febbe9abd4fc79d846a76acd70d063f
WJASK90 <jaskot.w92@gmail.com>
2025-05-13 16:22:45 +0200

    testy

---
+++ testy_Udemy_100_days/day_01.py
@@ -0,0 +1,6 @@
+#bandname
+
+welcome= print("Welcome to the Band Name Generator!")
+city = input("What's the name of the city you grew up in? " + '\n')
+pet = input("What's your pet's name? " + '\n')
+band_name = print("Your band name could be: " + city + " " + pet)
